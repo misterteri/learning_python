1aadd8eb02f752c3acb2775af207db99712461c2
misterteri <marcelinogilbert26@gmail.com>
2022-07-13 11:36:35 +0800

    pylint -> readability of the code, how to print the doc_string

---
+++ doc_printing.py
@@ -0,0 +1,10 @@
+# %%
+from main import addition
+
+print(addition.__annotations__)
+# %%
+print(addition.__doc__)
+# %%
+
+
+# example command = python doc_printing.py
--- main.py
+++ main.py
@@ -1,24 +1,26 @@
+"""Practice Python 3.10"""
+
 import argparse
-from audioop import mul
-from typing import Callable
 
 
 def addition(first_number: int | float, second_number: int | float) -> int | float:
+    """addition() receives two numbers and returns the sum of them"""
     return first_number + second_number
 
 
+def multiplication(first_number: int | float, second_number: int | float) -> int | float:
+    """multiplication() receives two numbers and returns the product of them"""
+    return first_number * second_number
 # lambda function # to make the funtion to a one lineR
 # multiplication = lambda first_number, second_number: first_number * second_number
-multiplication: Callable[[int | float, int | float], int |
-                         float] = lambda first_number, second_number: first_number * second_number
 # def addition and multiplication are the ways to declare a function in python
 
-
-def division(first_number: int | float, second_number: int | float) -> int | float:
-    return first_number / second_number
+# multiplication: Callable[[int | float, int | float], int | float]
+# = lambda first_number, second_number: first_number * second_number
 
 
 def main():
+    """ list of arguments """
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-f",  # names to be called to insert the number into
@@ -50,9 +52,6 @@ def main():
     elif args.action == "multiplication":
         result = multiplication(args.first_number, args.second_number)
 
-    elif args.action == "/":
-        result = division(args.first_number, args.second_number)
-
     print(result)
 
 
@@ -62,3 +61,5 @@ if __name__ == "__main__":
 # example input =  python Intro_1.py -a multiplication -f 3 -s 5
 # links
 # https://docs.python.org/3/howto/argparse.html
+
+# pylint to score and improve the readability of the code
