aae0b9dc2ac815d62fcdda41f135f311cd79f2d3
misterteri <marcelinogilbert26@gmail.com>
2022-07-13 14:16:02 +0800

    replace brackets #1

--- string_manipulation.py
+++ string_manipulation.py
@@ -1,18 +1,20 @@
 """String manipulation tutorial"""
 
 from copy import deepcopy
+from operator import truediv
 
 
 def main():
     """Main driver."""
-    alphabet: str = "abcabc"
-    print(alphabet.find("abcabc"))
-    # to make the new object 'new' independent to 'alphabet'
-    new = deepcopy(alphabet)
-    new = new + "abc"
-    print(alphabet)
-    print(new)
+    brackets = "{[()]}"
+    brackets2 = "([{}])"
+    result = brackets2.replace("{", "").replace("}", "").replace(
+        "(", "").replace(")", "").replace("[", "").replace("]", "")
+    print(result)
+    if len(result) == 0:
+        return True
+    return False
 
 
 if __name__ == "__main__":
-    main()
+    print(main())
