0b08bb0924a4dd7e2ec7fe7aa7ad22c2768c8809
misterteri <marcelinogilbert26@gmail.com>
2022-07-13 10:44:03 +0800

    multiplication and addition with argument parsing

--- Intro_1.py
+++ Intro_1.py
@@ -1,15 +1,24 @@
 import argparse
+from audioop import mul
+from typing import Callable
 
 
 def addition(first_number: int | float, second_number: int | float) -> int | float:
     return first_number + second_number
 
 
+# lambda function # to make the funtion to a one lineR
+# multiplication = lambda first_number, second_number: first_number * second_number
+multiplication: Callable[[int | float, int | float], int |
+                         float] = lambda first_number, second_number: first_number * second_number
+# def addition and multiplication are the ways to declare a function in python
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-f",
-        "--first_number",
+        "-f",  # names to be called to insert the number into
+        "--first_number",  # names to be called to insert the number into
         metavar="first_number",
         type=int,
         default=0
@@ -21,10 +30,26 @@ def main():
         type=int,
         default=0
     )
+    parser.add_argument(
+        "-a",
+        "--action",
+        metavar="action",
+        type=str,
+        default=0
+    )
+
     args = parser.parse_args()
-    result = addition(args.first_number, args.second_number)
+
+    if args.action == "addition":
+        result = addition(args.first_number, args.second_number)
+
+    elif args.action == "multiplication":
+        result = multiplication(args.first_number, args.second_number)
+
     print(result)
 
 
 if __name__ == "__main__":
     main()
+
+# example input =  python Intro_1.py -a multiplication -f 3 -s 5
