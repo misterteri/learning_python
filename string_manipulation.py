31a5bf2709016fac129bd9bbce58af412deb3c0e
misterteri <marcelinogilbert26@gmail.com>
2022-07-13 15:26:02 +0800

    PROPER Brackets Balance Checker

--- string_manipulation.py
+++ string_manipulation.py
@@ -1,20 +1,44 @@
 """String manipulation tutorial"""
 
+from cgi import test
 from copy import deepcopy
 from operator import truediv
 
 
+def checker1(test_brackets):
+    """"Brackets balance checker #1"""
+    old_len = len(test_brackets)
+    result = deepcopy(test_brackets)
+    while True:
+        result = result.replace(
+            "()", "").replace("[]", "").replace("{}", "")
+        if len(result) != old_len:
+            old_len = len(result)
+        elif len(result) == 0:
+            return True
+        else:
+            return False
+
+
+def checker2(brackets):
+    """Check if brackets are balanced."""
+    while True:
+        if '()' in brackets:
+            brackets = brackets.replace("()", "")
+        elif '{}' in brackets:
+            brackets = brackets.replace("{}", "")
+        elif '[]' in brackets:
+            brackets = brackets.replace("[]", "")
+        else:
+            return not brackets
+
+
 def main():
-    """Main driver."""
-    brackets = "{[()]}"
-    brackets2 = "([{}])"
-    result = brackets2.replace("{", "").replace("}", "").replace(
-        "(", "").replace(")", "").replace("[", "").replace("]", "")
-    print(result)
-    if len(result) == 0:
-        return True
-    return False
+    """Main driver"""
+    example = ("{{(({[]}))}")
+    print(checker1(example))
+    print(checker2(example))
 
 
 if __name__ == "__main__":
-    print(main())
+    main()
